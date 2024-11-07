072ff876e4e0bf19ae9f13fa83838b034f8ede6d
Felipe Arruda <felipesouzaero@gmail.com>
2024-11-07 00:41:05 -0300

    feat: adjusts in services

--- app/(project)/landing-page/components/footer.tsx
+++ app/(project)/landing-page/components/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <footer className="bg-[#0D0D0D] text-white pt-8 pb-5 px-4 md:px-16">
       <div>
         <div className="w-full mx-auto grid grid-cols-1 md:grid-cols-4 gap-8">
-          <div className="flex flex-col">
+          <div className="flex flex-col w-full md:w-2/3">
             <div className="flex items-center mb-7">
               <img src="/landing-page/logo.png" alt="Logo" className="h-10 mr-2" />
               <h2 className="text-xl font-bold text-white">
@@ -14,12 +14,10 @@ export default function Footer() {
               </h2>
             </div>
             <p className="text-[10px] text-[#B2B2B2] mb-7">
-              A perfect place for perfect food, all kinds of parties and any other
-              gathering as well, for our guests to simply unwind and relax in complete
-              serenity.
+              Eternize os momentos ao lado de quem você ama com um contador de tempo especial que celebra cada instante compartilhado. Celebre cada momento com quem é importante para você — seja amigos, avós, pais, namorado(a) ou até mesmo seu pet — criando um registro único que simboliza seu carinho e conexão.
             </p>
 
-            <div className="flex space-x-4">
+            {/* <div className="flex space-x-4">
               <a
                 href="#"
                 className="p-2 bg-[#29282d] rounded-full hover:bg-[#EF5DA8] transition"
@@ -44,7 +42,7 @@ export default function Footer() {
               >
                 <FiFacebook className="text-xl" />
               </a>
-            </div>
+            </div> */}
           </div>
 
           <div className="flex flex-col">
@@ -52,22 +50,7 @@ export default function Footer() {
             <ul className="space-y-2">
               <li>
                 <a href="#" className="text-[#B3B3B3] text-[10px]  hover:text-white">
-                  Menu
-                </a>
-              </li>
-              <li>
-                <a href="#" className="text-[#B3B3B3] text-[10px] hover:text-white">
-                  Gallery
-                </a>
-              </li>
-              <li>
-                <a href="#" className="text-[#B3B3B3] text-[10px] hover:text-white">
-                  Contact
-                </a>
-              </li>
-              <li>
-                <a href="#" className="text-[#B3B3B3] text-[10px] hover:text-white">
-                  Order Now
+                  Serviço
                 </a>
               </li>
             </ul>
@@ -97,17 +80,12 @@ export default function Footer() {
 
           <div className="flex flex-col">
             <h3 className="font-bold text-xs text-white mb-4">Contact</h3>
-            {/* <p className="text-[#B3B3B3] text-[10px] mb-3">
-              <strong className="text-white">Call:</strong> +00 0000000000
-            </p> */}
             <p className="text-[#B3B3B3] text-[10px] mb-3">
               <strong className="text-white">Email:</strong> contato@voceeespecial.com.br
-
             </p>
-            {/* <p className="text-[#B3B3B3] text-[10px]">
-              <strong className="text-white">Address:</strong> LoveSafe, Lorem Ipsum, Lorem lorem, 252, Lorem -
-              cidade
-            </p> */}
+            <p className="text-[#B3B3B3] text-[10px] mb-3">
+              <strong className="text-white">Cnpj:</strong> 58.007.256/0001-00
+            </p>
           </div>
         </div>
 
@@ -115,9 +93,9 @@ export default function Footer() {
 
       <hr className="border-t border-gray-700 mt-10" />
 
-      <div className="flex justify-center items-center text-center text-[#B3B3B3] text-[10px] text-sm my-5">
+      <div className="flex justify-center items-center text-center text-[#B3B3B3] text-[10px] md:text-sm my-5">
         <p className="h-full flex justify-center items-center">
-          Copyright © 2024 LoveSafe. All Rights Reserved. Designed by Izabely Almeida
+          Copyright © 2024 voceeespecial. All Rights Reserved. Designed by Izabely Almeida
         </p>
       </div>
     </footer>
